c5fe137d427bc944e55b106acb9f515812bda0f2
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-02-10 12:43:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const daysOfAYear = (year) => {
+	if (year%100===0) {
+		if (year%400===0) {
+			return 366;
+		} else {
+			return 365;
+		}
+	} else {
+		if (year%4===0) {
+			return 366;
+		} else {
+			return 365;
+		}
+	}
+}
+
+const year = prompt("Enter the year");
+alert(daysOfAYear(year));
